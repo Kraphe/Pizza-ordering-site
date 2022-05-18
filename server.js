54eeb053b1c8f3cdcb23d5f918f9999fd7dd62dc
Kraphe <kraphe001@gmail.com>
2022-05-18 17:33:25 +0530

    initial

---
+++ server.js
@@ -0,0 +1,25 @@
+const express=require('express');
+
+const app=express();
+
+const ejs=require('ejs');
+
+const path= require('path');   // this module genrate path  this is inbuild module no need to install
+
+const expressLayout=require('express-ejs-layouts');
+
+
+const PORT=process.env.PORT||3000;
+
+app.get('/',(req,res)=>{
+    res.render('home');  //ejs file name you wont to render on web  page 
+})
+
+// set Template engine
+app.use( expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'))   //this will tell express where we have kept views file 
+app.set('view engine','ejs');
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+})
